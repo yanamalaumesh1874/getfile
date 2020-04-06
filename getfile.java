cffbae8e92741f447403e0d8055d6450c78546ca
yanamalaumesh1874 <62681339+yanamalaumesh1874@users.noreply.github.com>
2020-04-06 20:56:36 +0530

    Create getfile.java

---
+++ getfile.java
@@ -0,0 +1,19 @@
+import java.io.File;
+public class getfile { 
+  public static void main(String[] args)
+	{
+    File myObj = new File("D:// filename.txt");
+    if (myObj.exists()) 
+	{
+      System.out.println("File name: " + myObj.getName());
+      System.out.println("Absolute path: " + myObj.getAbsolutePath());
+      System.out.println("Writeable: " + myObj.canWrite());
+      System.out.println("Readable " + myObj.canRead());
+      System.out.println("File size in bytes " + myObj.length());
+    }
+	else
+	{
+      System.out.println("The file  not exist");
+    }
+  }
+}
